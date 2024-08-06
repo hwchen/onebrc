eb7033f36257998a9acfcad93a53d9502ee41e89
Walther Chen <walther.chen@gmail.com>
2024-08-06 12:23:49 -0400

    zig count lines

--- justfile
+++ justfile
@@ -2,4 +2,4 @@ run:
     odin build . -o:aggressive && time ./onebrc measurement_data.txt
 
 run-zig:
-    zig build-exe main.zig -Doptimize=ReleaseFast -femit-bin=onebrc-zig && time ./onebrc-zig measurement.txt
+    zig build-exe main.zig -Doptimize=ReleaseFast -femit-bin=onebrc-zig && time ./onebrc-zig measurement_data.txt
--- main.zig
+++ main.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("hello world", .{});
+    var args = std.process.args();
+    _ = args.next();
+    const path = args.next().?;
+    const f = try std.fs.cwd().openFile(path, .{});
+    const rdr = f.reader();
+    var brdr = std.io.bufferedReaderSize(4096 * 8, rdr);
+    const input = brdr.reader();
+
+    var idx: usize = 0;
+    var buf: [64]u8 = undefined;
+    while (true) {
+        const line_maybe = try input.readUntilDelimiterOrEof(&buf, '\n');
+        if (line_maybe) |_| {
+            idx += 1;
+        } else {
+            break;
+        }
+    }
+    std.debug.print("{d}\n", .{idx});
 }
